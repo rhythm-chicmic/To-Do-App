45d314a238445c3cc8c003416442bf9a0587e825
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-03-21 19:08:38 +0530

    minor commit

--- src/app/Authentication/sign-up/sign-up.component.ts
+++ src/app/Authentication/sign-up/sign-up.component.ts
@@ -7,6 +7,7 @@ import { AuthserviceService } from '../authservice.service';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+  submitted:boolean =false;
   SignupForm = new FormGroup({
     name: new FormControl('', [Validators.required,Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
@@ -24,10 +25,17 @@ export class SignUpComponent {
 
   OnSubmit() {
     console.log(this.SignupForm.value)
+    if(this.SignupForm.valid){
+    
     this.service.PostSignUp(this.SignupForm.value).subscribe((res) => {
       console.log(res)
     });
   }
+  else {
+    this.submitted=true;
+  }
+  
+}
 
 
 }
