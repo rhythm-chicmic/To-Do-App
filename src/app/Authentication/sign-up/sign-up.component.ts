ddf5ec47b2b9b58d630756b3de5702ab04ca7256
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-03-21 18:47:07 +0530

    minor commit

--- src/app/Authentication/sign-up/sign-up.component.ts
+++ src/app/Authentication/sign-up/sign-up.component.ts
@@ -8,7 +8,7 @@ import { AuthserviceService } from '../authservice.service';
 })
 export class SignUpComponent {
   SignupForm = new FormGroup({
-    name: new FormControl('', Validators.required),
+    name: new FormControl('', [Validators.required,Validators.minLength(3)]),
     email: new FormControl('', [Validators.required, Validators.email, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]),
     phoneNumber: new FormControl('', [Validators.required, Validators.maxLength(10),
     Validators.pattern('^[0-9]+$')]),
